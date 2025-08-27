f9dd30f283a360298dd54a2392bab5826f2cc1ed
SMC242 <56912943+SMC242@users.noreply.github.com>
2025-08-27 01:40:15 +0100

    feat: domain type

--- src/domain.rs
+++ src/domain.rs
@@ -1,49 +1,42 @@
-use std::fmt::Display;
+use std::{collections::HashSet, str::FromStr};
 
-// DNS zones are a tree that can be traversed up to recreate
-// the domain name
-// See https://en.wikipedia.org/wiki/Domain_Name_System#Structure
-pub struct ZoneTree {
+/// Get the DNS zones in top-down order (I.E TLD first)
+pub fn to_zones(domain: &str) -> impl Iterator<Item = &str> {
+    domain.split('.').rev()
+}
+
+pub struct Domain {
     name: String,
-    parent: Option<Box<ZoneTree>>,
-    children: Vec<Box<ZoneTree>>,
+    zones: Vec<String>,
 }
 
-impl ZoneTree {
-    pub fn new(name: String) -> Self {
+impl Domain {
+    pub fn new(domain: &str) -> Self {
         Self {
-            name,
-            parent: None,
-            children: vec![],
+            name: domain.to_string(),
+            zones: to_zones(domain).map(str::to_string).collect(),
         }
-    }
-
-    pub fn add(root: &mut Self, name: String) {
-        let segments = name.split('.').rev();
-
-        // The root has name = ""
-        let mut children = root.children;
-        let mut child = children[0];
-        for seg in segments {
-            for child in children {
-                if child.name == seg {
-                    children = child.children;
-                }
-            }
-        }
-
-        // This is wrong. I need to add the segment if it's not already there at each step
-        child.children.push(Box::new(ZoneTree {
-            name: seg,
-            parent: Box::new(child),
-            children: vec![],
-        }))
     }
 }
 
-impl Display for ZoneTree {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // Traverse up the tree
-        todo!()
+pub enum DomainParseError {
+    EmptyString,
+    InvalidFormat,
+}
+
+impl FromStr for Domain {
+    type Err = DomainParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let segments: Vec<String> = to_zones(s).map(str::to_string).collect();
+
+        match segments.len() {
+            0 => Err(DomainParseError::EmptyString),
+            1 => Err(DomainParseError::InvalidFormat),
+            _ => Ok(Self {
+                name: s.to_string(),
+                zones: segments,
+            }),
+        }
     }
 }
